2fe21e2467249dc9ce561fbfc429bc20383cee66
Peter Jankuliak <p.jankuliak@gmail.com>
2017-08-21 12:35:12 +0200

    Better 'dispatch' function

--- src/dispatch.h
+++ src/dispatch.h
@@ -6,36 +6,22 @@
 // in the event_base loop.
 template<class F>
 static void dispatch(event_base* evbase, F f) {
-    // TODO(peterj) Check out the event_active function to do this (might be
-    // more efficient).
-
-    using namespace std;
-
     struct DispatchCtx {
         F callback;
         struct event* ev;
     
         static void cb(evutil_socket_t, short, void *param) {
             auto ctx = static_cast<DispatchCtx*>(param);
-            auto cb = move(ctx->callback);
+            auto cb = std::move(ctx->callback);
             event_free(ctx->ev);
             delete ctx;
             cb();
         }
     };
 
-    struct timeval zero_sec;
-
-    zero_sec.tv_sec = 0;
-    zero_sec.tv_usec = 0;
-
-    auto ctx = new DispatchCtx{move(f)};
-
+    auto ctx = new DispatchCtx{std::move(f)};
     struct event* ev = event_new(evbase, -1, EV_PERSIST, DispatchCtx::cb, ctx);
-
     ctx->ev = ev;
-
-    int add_result = event_add(ev, &zero_sec);
-    assert(add_result == 0);
+    event_active(ev, 0, 0);
 }
 
