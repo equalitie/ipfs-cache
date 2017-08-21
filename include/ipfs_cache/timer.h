ff3b8a362117e6b7a1c3857252843dcc990450e9
Peter Jankuliak <p.jankuliak@gmail.com>
2017-08-21 17:45:00 +0200

    Move Timer function definitions from .h to .cpp

--- include/ipfs_cache/timer.h
+++ include/ipfs_cache/timer.h
@@ -2,6 +2,7 @@
 
 #include <event2/event_struct.h>
 #include <chrono>
+#include <functional>
 
 namespace ipfs_cache {
 
@@ -31,64 +32,5 @@ private:
     event* _event;
     std::function<void()> _cb;
 };
-
-inline
-Timer::Timer(struct event_base* evbase)
-    : _evbase(evbase)
-{ }
-
-inline
-bool Timer::is_running() const
-{
-    return _event != nullptr;
-}
-
-inline
-void Timer::stop()
-{
-    if (!_event) return;
-    evtimer_del(_event);
-    event_free(_event);
-    _event = nullptr;
-    _cb = nullptr;
-}
-
-inline
-void Timer::start(Clock::duration duration, std::function<void()> cb)
-{
-    if (is_running()) {
-        stop();
-    }
-
-    using namespace std::chrono;
-
-    auto timeout_ms = duration_cast<milliseconds>(duration).count();
-
-    struct timeval timeout;
-
-    timeout.tv_sec  = timeout_ms / 1000;
-    timeout.tv_usec = (timeout_ms % 1000) * 1000;
-
-    _cb = std::move(cb);
-
-    _event = event_new(_evbase, -1, 0, Timer::call, this);
-
-    evtimer_add(_event, &timeout);
-}
-
-inline
-void Timer::call(evutil_socket_t, short, void * v)
-{
-    auto self = (Timer*) v;
-    auto cb = std::move(self->_cb);
-    self->stop();
-    cb();
-}
-
-inline
-Timer::~Timer()
-{
-    stop();
-}
 
 } // ipfs_cache namespace
